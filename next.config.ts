92808f92689ffdf795b3a6cc71fb063fc78b2105
Pablo <jpcardonab0520@gmail.com>
2025-05-09 10:02:38 -0500

    chore: remove unused imports and comments from layout and configuration files

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  
+  // Suppressions for webpack warnings
+  webpack: (config, { isServer }) => {
+    // Suppress the warning about the punycode module
+    config.ignoreWarnings = [
+      { module: /node_modules\/punycode/ }
+    ];
+    
+    return config;
+  },
 };
 
 export default nextConfig;
